cae43b76ecf94ac5bf88224b30d50433ad0c7ccc
Brian Bansenauer <brianb@cascadia.edu>
2014-12-15 21:18:34 -0800

    Initial Commit

---
+++ CascadiaDirections/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CascadiaDirections
+//
+//  Created by Brian Bansenauer on 12/15/14.
+//  Copyright (c) 2014 Cascadia College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
